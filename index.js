9f929a1b7bb5a1ce5f116fd620beeb78947a372f
Jasmin <ssongnari@gmail.com>
2023-08-07 12:24:36 -0400

    commit tree 1c22862fbce7a33571ea5f939a18345196301931

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+writeCards(names, "birthday");
+
+function countDown(number) {
+  while (number > 0) {
+    console.log(number);
+    number -= 1
+  }
+
+  console.log(number)
+}
